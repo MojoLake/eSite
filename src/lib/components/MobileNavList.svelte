56e0d68aa01d60fb8ecb0fd63e6b9264473359b0
Elias Simojoki <elias@hamina.com>
2025-09-26 18:55:36 +0300

    Make nav height constant

--- src/lib/components/MobileNavList.svelte
+++ src/lib/components/MobileNavList.svelte
@@ -1,23 +1,24 @@
 <script>
-    import NavList from '$lib/components/NavList.svelte';
+  import NavList from "$lib/components/NavList.svelte";
 
-    export let onClick;
+  export let onClick;
 </script>
 
-<style>
-    .mobile-nav {
-        --nav-height: calc(100dvh - 12rem);
-    
-        --nav-ul-display: flex;
-        --nav-ul-flex-direction: column;
-        --nav-ul-align-items: center;
-        --nav-ul-justify-content: space-evenly;
-        --nav-ul-height: 100%;
-        
-        --nav-li-font-size: 3rem;
-    }
-</style>
-
 <div class="mobile-nav">
-    <NavList onClick={onClick}/>
+  <NavList {onClick} />
 </div>
+
+<style>
+  .mobile-nav {
+    /* --nav-height: calc(100dvh - 12rem); */
+    --nav-height: 70vh;
+
+    --nav-ul-display: flex;
+    --nav-ul-flex-direction: column;
+    --nav-ul-align-items: center;
+    --nav-ul-justify-content: space-evenly;
+    --nav-ul-height: 100%;
+
+    --nav-li-font-size: 3rem;
+  }
+</style>
