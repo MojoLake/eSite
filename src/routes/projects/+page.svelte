d37a2c9090ead2b0313b46291674e537506d83ea
Elias Simojoki <elias@hamina.com>
2025-09-26 15:40:36 +0300

    Fix projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,11 +1,14 @@
 <style>
-    a:hover {
-        color: #f0f0f0;
-    }
+  p {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+
+    margin: 0 auto;
+  }
 </style>
 
 <p>
     This page is to be done. 
-    For now just check my 
-    <a href="https://github.com/MojoLake">github</a>
+    For now just check my <a href="https://github.com/MojoLake">Github.</a>
 </p>
